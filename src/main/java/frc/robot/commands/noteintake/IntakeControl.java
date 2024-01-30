34b814840d26300a1ad00956e82d2d899c1e8968
FRC9149 <FRC9149@rfsd.k12.wi.us>
2024-01-30 16:05:23 -0600

    add init

---
+++ src/main/java/frc/robot/commands/noteintake/IntakeControl.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.noteintake;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
+
+
+public class IntakeControl extends Command
+{
+
+static CANSparkMax GearControl = new CANSparkMax(0, MotorType.kBrushless);
+
+ public static void jksdfh() {
+    GearControl.set(1);
+
+
+
+ }
+
+}
+    
+
