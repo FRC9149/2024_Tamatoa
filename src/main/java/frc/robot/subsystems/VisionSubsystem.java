7b4f86a553d55797ca4d7c30512da69d67a8c31b
Arcbound49 <john.alden.pearson49@gmail.com>
2024-01-31 16:20:29 -0600

    Added vision subsystem file

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class VisionSubsystem {
+    public static final NetworkTable Table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    public static final double cameraHeight = 0;
+    public static final double cameraXAngle = 0;
+    public static final double cameraYAngle = 0;
+
+    public static void init() {
+        Table.getEntry("pipeline").setNumber(0);
+        Table.getEntry("camMode").setNumber(0);
+    }
+}
