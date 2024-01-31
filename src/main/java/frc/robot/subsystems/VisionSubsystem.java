0a562efbcd98f2ca16cbe79b45491730ad3c3c5d
Arcbound49 <john.alden.pearson49@gmail.com>
2024-01-31 16:46:11 -0600

    Added vision system to get distance and get pose2d to note/object point

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
@@ -13,5 +16,21 @@ public class VisionSubsystem {
     public static void init() {
         Table.getEntry("pipeline").setNumber(0);
         Table.getEntry("camMode").setNumber(0);
-    }    
+    }
+
+    public static Pose2d PickUpNote() {
+
+        Pose2d returnPose;
+
+        Rotation2d rotation = Rotation2d.fromDegrees(Table.getEntry("tx").getDouble(0) - cameraXAngle);
+        Translation2d translation = new Translation2d(Distance(0), rotation);
+
+        returnPose = new Pose2d(translation, rotation);
+
+        return returnPose;
+    }
+
+    private static double Distance(double height) {
+        return (Math.abs(height - cameraHeight)/Math.tan(cameraYAngle+Table.getEntry("ty").getDouble(0)));
+    }
 }
